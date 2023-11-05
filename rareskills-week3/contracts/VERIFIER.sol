3017bd7e48d76addb156537dd92c8cc5dc6d384c
Sam Polgar <sampolgar@gmail.com>
2023-11-05 15:59:24 +1100

    playing with imports

--- rareskills-week3/contracts/VERIFIER.sol
+++ rareskills-week3/contracts/VERIFIER.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import "https://github.com/witnet/elliptic-curve-solidity/blob/master/contracts/EllipticCurve.sol"
+// import "https://github.com/witnet/elliptic-curve-solidity/blob/master/contracts/EllipticCurve.sol"
+
+// import "@elliptic-curve-solidity/EllipticCurve.sol";
+// import "./EllipticCurve.sol";
+// import "@witnet/EllipticCurve.sol";
 
 contract Verifier {
     function add(uint256 x1, uint256 y1, uint256 x2, uint256 y2) public view returns (uint256 x, uint256 y) {
@@ -13,6 +17,25 @@ contract Verifier {
         (bool ok, bytes memory result) = address(7).staticcall(abi.encode(x1, y1, scalar));
         require(ok, "mul failed");
         (x, y) = abi.decode(result, (uint256, uint256));
+    }
+
+    /// @dev Modular euclidean inverse of a number (mod p).
+    /// @param _x The number
+    /// @param _pp The modulus
+    /// @return q such that x*q = 1 (mod _pp)
+    function invMod(uint256 _x, uint256 _pp) public pure returns (uint256) {
+        require(_x != 0 && _x != _pp && _pp != 0, "Invalid number");
+        uint256 q = 0;
+        uint256 newT = 1;
+        uint256 r = _pp;
+        uint256 t;
+        while (_x != 0) {
+            t = r / _x;
+            (q, newT) = (newT, addmod(q, (_pp - mulmod(t, newT, _pp)), _pp));
+            (r, _x) = (_x, r - t * _x);
+        }
+
+        return q;
     }
 
     struct ECPoint {
@@ -30,7 +53,7 @@ contract Verifier {
 
         // to calculate G1 * num/den, change den into multiplicative inverse i.e. pow(den, -1, curve_order)
         uint256 curve_order = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
-        uint256 denInv = modularMultiplicativeInverse(den, curve_order);
+        uint256 denInv = EllipticCurve.invMod(den, curve_order);
         uint256 scalar = num * denInv;
         (uint256 x2, uint256 y2) = mul(1, 2, scalar);
 
@@ -38,34 +61,5 @@ contract Verifier {
         require(y1 == y2, "y1 y2 failed");
         if (x1 != x2 && y1 != y2) return false;
         return true;
-    }
-
-    // Helper function to safely subtract two unsigned integers
-    function safeSubtract(uint256 a, uint256 b) internal pure returns (uint256, bool) {
-        if (a < b) {
-            // Would underflow
-            return (0, false);
-        } else {
-            return (a - b, true);
-        }
-    }
-
-    // Extended Euclidean Algorithm adapted for unsigned integers.
-    function extendedEuclidean(uint256 a, uint256 b) public pure returns (uint256, uint256, uint256) {
-        if (a == 0) {
-            return (b, 0, 1);
-        } else {
-            (uint256 gcd, uint256 x1, uint256 y1) = extendedEuclidean(b % a, a);
-            (uint256 x, bool subtracted) = safeSubtract(y1, (b / a) * x1);
-            require(subtracted, "Operation would underflow");
-            return (gcd, x, x1); // Note that y = x1, since subtraction cannot be negative here.
-        }
-    }
-
-    // Computes the modular multiplicative inverse of a modulo m.
-    function modularMultiplicativeInverse(uint256 a, uint256 m) public pure returns (uint256) {
-        (uint256 gcd, uint256 x, uint256 y) = extendedEuclidean(a, m);
-        require(gcd == 1, "Inverse doesn't exist");
-        return x; // x is already in the range 0 to m-1
     }
 }
