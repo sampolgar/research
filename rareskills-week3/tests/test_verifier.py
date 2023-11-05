8c1eee348f5e24cdc0b84a94e609bc28e9f94dd0
Sam Polgar <sampolgar@gmail.com>
2023-11-06 08:31:38 +1100

    testing on EC points working

--- rareskills-week3/tests/test_verifier.py
+++ rareskills-week3/tests/test_verifier.py
@@ -3,42 +3,38 @@ from fractions import Fraction
 
 
 def test_rational_add_easy(verifier_contract):
-    frac1 = Fraction(123, 456)
-    frac2 = Fraction(789, 123)
-    frac_add = frac1 + frac2
-    frac_add_num = frac_add.numerator
-    frac_add_den = frac_add.denominator
+    frac_add = Fraction(123, 456) + Fraction(789, 123)
 
     scalar1 = 123 * pow(456, -1, curve_order) % curve_order
     scalar2 = 789 * pow(123, -1, curve_order) % curve_order
+
     hidden_ecp1 = multiply(G1, scalar1)
     hidden_ecp2 = multiply(G1, scalar2)
 
-    A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
-    B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
+    # Prepare the points for addition
+    point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
+    point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
 
-    res = verifier_contract.rationalAdd(
-        A, B, frac_add_num, frac_add_den)
-    print("verifier_res: ", res)
-    assert res == True
+    # Add the points on the curve and check if the result is as expected
+    assert verifier_contract.rationalAdd(
+        point_A, point_B, frac_add.numerator, frac_add.denominator) == True
+    print("Verification successful.")
 
 
 def test_rational_add_harder(verifier_contract):
-    frac1 = Fraction(123456, 456789)
-    frac2 = Fraction(789123, 123456)
-    frac_add = frac1 + frac2
-    frac_add_num = frac_add.numerator
-    frac_add_den = frac_add.denominator
+    frac_add = Fraction(123456, 456789) + Fraction(789123, 123456)
 
+    # Calculate scalars and ECC point addition
     scalar1 = 123456 * pow(456789, -1, curve_order) % curve_order
     scalar2 = 789123 * pow(123456, -1, curve_order) % curve_order
     hidden_ecp1 = multiply(G1, scalar1)
     hidden_ecp2 = multiply(G1, scalar2)
 
-    A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
-    B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
+    # Prepare the points for addition
+    point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
+    point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
 
-    res = verifier_contract.rationalAdd(
-        A, B, frac_add_num, frac_add_den)
-    print("verifier_res: ", res)
-    assert res == True
+    # Add the points on the curve and check if the result is as expected
+    assert verifier_contract.rationalAdd(
+        point_A, point_B, frac_add.numerator, frac_add.denominator) == True
+    print("Verification successful.")
