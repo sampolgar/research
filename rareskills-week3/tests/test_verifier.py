78ec43de937951ebf5d7587ea50b4435d6661a1e
Sam Polgar <sampolgar@gmail.com>
2023-11-06 16:04:53 +1100

    week 3 finish

--- rareskills-week3/tests/test_verifier.py
+++ rareskills-week3/tests/test_verifier.py
@@ -4,42 +4,64 @@ from py_ecc.fields import FQ
 from fractions import Fraction
 import random
 
-# def test_rational_add_easy(verifier_contract):
-#     frac_add = Fraction(123, 456) + Fraction(789, 123)
 
-#     scalar1 = 123 * pow(456, -1, curve_order) % curve_order
-#     scalar2 = 789 * pow(123, -1, curve_order) % curve_order
+def test_rational_add_easy(verifier_contract):
+    frac_add = Fraction(123, 456) + Fraction(789, 123)
 
-#     hidden_ecp1 = multiply(G1, scalar1)
-#     hidden_ecp2 = multiply(G1, scalar2)
+    scalar1 = 123 * pow(456, -1, curve_order) % curve_order
+    scalar2 = 789 * pow(123, -1, curve_order) % curve_order
 
-#     # Prepare the points for addition
-#     point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
-#     point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
+    hidden_ecp1 = multiply(G1, scalar1)
+    hidden_ecp2 = multiply(G1, scalar2)
 
-#     # Add the points on the curve and check if the result is as expected
-#     assert verifier_contract.rationalAdd(
-#         point_A, point_B, frac_add.numerator, frac_add.denominator) == True
-#     print("Verification successful.")
+    # Prepare the points for addition
+    point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
+    point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
+
+    # Add the points on the curve and check if the result is as expected
+    assert verifier_contract.rationalAdd(
+        point_A, point_B, frac_add.numerator, frac_add.denominator) == True
+    print("Verification successful.")
 
 
-# def test_rational_add_harder(verifier_contract):
-#     frac_add = Fraction(123456, 456789) + Fraction(789123, 123456)
+def test_rational_add_harder(verifier_contract):
+    frac_add = Fraction(123456, 456789) + Fraction(789123, 123456)
 
-#     # Calculate scalars and ECC point addition
-#     scalar1 = 123456 * pow(456789, -1, curve_order) % curve_order
-#     scalar2 = 789123 * pow(123456, -1, curve_order) % curve_order
-#     hidden_ecp1 = multiply(G1, scalar1)
-#     hidden_ecp2 = multiply(G1, scalar2)
+    # Calculate scalars and ECC point addition
+    scalar1 = 123456 * pow(456789, -1, curve_order) % curve_order
+    scalar2 = 789123 * pow(123456, -1, curve_order) % curve_order
+    hidden_ecp1 = multiply(G1, scalar1)
+    hidden_ecp2 = multiply(G1, scalar2)
 
-#     # Prepare the points for addition
-#     point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
-#     point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
+    # Prepare the points for addition
+    point_A = (hidden_ecp1[0].n, hidden_ecp1[1].n)
+    point_B = (hidden_ecp2[0].n, hidden_ecp2[1].n)
 
-#     # Add the points on the curve and check if the result is as expected
-#     assert verifier_contract.rationalAdd(
-#         point_A, point_B, frac_add.numerator, frac_add.denominator) == True
-#     print("Verification successful.")
+    # Add the points on the curve and check if the result is as expected
+    assert verifier_contract.rationalAdd(
+        point_A, point_B, frac_add.numerator, frac_add.denominator) == True
+    print("Verification successful.")
+
+
+def test_matrix_vector_multiplication_easy(verifier_contract):
+    n = 2
+    matrix = [1, 2, 1, 2]
+    s = [(1, 2), (1, 2)]
+    o = [3353031288059533942658390886683067124040920775575537747144343083137631628272,  19321533766552368860946552437480515441416830039777911637913418824951667761761,
+         3353031288059533942658390886683067124040920775575537747144343083137631628272,  19321533766552368860946552437480515441416830039777911637913418824951667761761]
+    assert verifier_contract.matmul(matrix, n, s, o) == True
+
+
+def test_matrix_vector_multiplication_hard(verifier_contract):
+    n = 3
+    matrix = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    s = [(1, 2), (1, 2), (1, 2)]
+    o = [
+        4503322228978077916651710446042370109107355802721800704639343137502100212473, 6132642251294427119375180147349983541569387941788025780665104001559216576968,
+        20620327752371756597889511849668302065574790742892641857779427155670977738300, 13476221886639441297190182883126933680754442408693165714792516739857175455715,
+        20453939078259811958859768391452073654460321168773748684493785442363495374770, 9582859829925552874957318860636821932456214701004608986274201852321144884827
+    ]
+    assert verifier_contract.matmul(matrix, n, s, o) == True
 
 
 # def test_matrix_vector_multiplication(verifier_contract):
@@ -75,24 +97,3 @@ import random
 #     print("result_converted", result_converted)
 #     assert verifier_contract.matmul(
 #         matrix_list, n, vector, result_converted) == True
-
-
-def test_matrix_vector_multiplication(verifier_contract):
-    n = 2
-    matrix = [1, 2, 1, 2]
-    s = [(1, 2), (1, 2)]
-    o = [3353031288059533942658390886683067124040920775575537747144343083137631628272,  19321533766552368860946552437480515441416830039777911637913418824951667761761,
-         3353031288059533942658390886683067124040920775575537747144343083137631628272,  19321533766552368860946552437480515441416830039777911637913418824951667761761]
-    assert verifier_contract.matmul(matrix, n, s, o) == True
-
-
-def test_matrix_vector_multiplication_2(verifier_contract):
-    n = 3
-    matrix = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    s = [(1, 2), (1, 2), (1,2)]
-    o = [
-        4503322228978077916651710446042370109107355802721800704639343137502100212473, 6132642251294427119375180147349983541569387941788025780665104001559216576968,
-        20620327752371756597889511849668302065574790742892641857779427155670977738300, 13476221886639441297190182883126933680754442408693165714792516739857175455715,
-        20453939078259811958859768391452073654460321168773748684493785442363495374770, 9582859829925552874957318860636821932456214701004608986274201852321144884827
-    ]
-    assert verifier_contract.matmul(matrix, n, s, o) == True
