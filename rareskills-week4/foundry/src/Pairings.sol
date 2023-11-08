33769410e1e83444e138f4957ea1dd3f51a63611
Sam Polgar <sampolgar@gmail.com>
2023-11-09 06:37:49 +1100

    pairing with 4

--- rareskills-week4/foundry/src/Pairings.sol
+++ rareskills-week4/foundry/src/Pairings.sol
@@ -5,25 +5,25 @@ contract Pairings {
     // @constant = curve order
     uint256 CURVE_ORDER = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
-    /// @dev ECPoint addition
-    /// @param x1 ECPoint1 x
-    /// @param y1 ECPoint1 y
-    /// @param x2 ECPoint2 x
-    /// @param y2 ECPoint2 y
-    /// @return x addition of x1 and x2 from precompile address 6 ECPoint x
-    /// @return y addition of y1 and y2 from precompile address 6 ECPoint y
+    /// @dev G1Point addition
+    /// @param x1 G1Point1 x
+    /// @param y1 G1Point1 y
+    /// @param x2 G1Point2 x
+    /// @param y2 G1Point2 y
+    /// @return x addition of x1 and x2 from precompile address 6 G1Point x
+    /// @return y addition of y1 and y2 from precompile address 6 G1Point y
     function add(uint256 x1, uint256 y1, uint256 x2, uint256 y2) internal view returns (uint256 x, uint256 y) {
         (bool ok, bytes memory result) = address(6).staticcall(abi.encode(x1, y1, x2, y2));
         require(ok, "add failed");
         (x, y) = abi.decode(result, (uint256, uint256));
     }
 
-    /// @dev ECPoint scalar multiplication using precompile 7
-    /// @param x1 ECPoint x
-    /// @param y1 ECPoint y
+    /// @dev G1Point scalar multiplication using precompile 7
+    /// @param x1 G1Point x
+    /// @param y1 G1Point y
     /// @param scalar is scalar multiple
-    /// @return x ECPoint x
-    /// @return y ECPoint y
+    /// @return x G1Point x
+    /// @return y G1Point y
     function mul(uint256 x1, uint256 y1, uint256 scalar) internal view returns (uint256 x, uint256 y) {
         (bool ok, bytes memory result) = address(7).staticcall(abi.encode(x1, y1, scalar));
         require(ok, "mul failed");
@@ -65,25 +65,72 @@ contract Pairings {
         return out[0] != 0;
     }
 
-    struct ECPoint {
+    struct G1Point {
         uint256 x;
         uint256 y;
     }
 
-    function createX(uint256[] memory input) public view returns (uint256, uint256) {
-        ECPoint memory pointSummary;
+    struct G2Point {
+        uint256[2] x;
+        uint256[2] y;
+    }
+
+    function createX_1(uint256[] memory input) public view returns (uint256, uint256) {
+        G1Point memory pointSummary;
 
         for (uint256 i = 0; i < input.length; i++) {
             //scalar mul G1 point with input
             // add to point at i
-            ECPoint memory iteratorPoint;
+            G1Point memory iteratorPoint;
             (iteratorPoint.x, iteratorPoint.y) = mul(1, 2, input[i]);
             (pointSummary.x, pointSummary.y) = add(pointSummary.x, pointSummary.y, iteratorPoint.x, iteratorPoint.y);
         }
         return (pointSummary.x, pointSummary.y);
     }
 
-    /// (4444740815889402603535294170722302758225367627362056425101568584910268024244, 10537263096529483164618820017164668921386457028564663708352735080900270541420)
+    function pairingVerifier(
+        G1Point memory A1,
+        G2Point memory B2,
+        G1Point memory Alpha1,
+        G2Point memory Beta2,
+        G1Point memory X1,
+        G2Point memory Gamma2,
+        G1Point memory C1,
+        G2Point memory Delta2
+    ) public view returns (bool) {
+        G1Point[4] memory p1 = [A1, Alpha1, X1, C1];
+        G2Point[4] memory p2 = [B2, Beta2, Gamma2, Delta2];
 
-    /// function verifyPairing(bytes calldata pairingInput, bytes calldata xInput) public view returns (bool) {}
+        uint256 inputSize = 24;
+        uint256[] memory input = new uint256[](inputSize);
+
+        // not sure why we do this?
+        for (uint256 i = 0; i < 4; i++) {
+            uint256 j = i * 6;
+            input[j + 0] = p1[i].x;
+            input[j + 1] = p1[i].y;
+            input[j + 2] = p2[i].x[0];
+            input[j + 3] = p2[i].x[1];
+            input[j + 4] = p2[i].y[0];
+            input[j + 5] = p2[i].y[1];
+        }
+
+        uint256[1] memory out;
+        bool success;
+
+        assembly {
+            success := staticcall(sub(gas(), 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success
+            case 0 { invalid() }
+        }
+
+        require(success, "pairing-opcode-failed");
+
+        return out[0] != 0;
+    }
 }
+
+/// (4444740815889402603535294170722302758225367627362056425101568584910268024244, 10537263096529483164618820017164668921386457028564663708352735080900270541420)
+
+/// function verifyPairing(bytes calldata pairingInput, bytes calldata xInput) public view returns (bool) {}
